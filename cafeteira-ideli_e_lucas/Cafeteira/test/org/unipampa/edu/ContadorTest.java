22f2a7449611007b312970c866401c63ae917bc5
Yury Alencar <yuryalencar19@gmail.com>
2021-07-05 21:24:24 -0300

    test(junit): refactor to use junit 5

--- cafeteira-ideli_e_lucas/Cafeteira/test/org/unipampa/edu/ContadorTest.java
+++ cafeteira-ideli_e_lucas/Cafeteira/test/org/unipampa/edu/ContadorTest.java
@@ -5,12 +5,12 @@
  */
 package org.unipampa.edu;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -21,19 +21,19 @@ public class ContadorTest {
     public ContadorTest() {
     }
     
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
     }
     
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
     }
     
-    @Before
+    @BeforeEach
     public void setUp() {
     }
     
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
